b0cee64cc023b2093efb7f726200d19c748f2702
dado93 <davide.marzorati.93@gmail.com>
2021-05-20 11:42:04 +0200

    Added Intersphinx support in Sphinx configuration file

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,15 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'sphinx_rtd_theme', 'sphinx.ext.napoleon']
+              'sphinx_rtd_theme', 'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx']
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None)
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
